a01cd5a974fe49421cc0740ba585eea7a80b27c5
Marc Baldischwieler <marc.baldi@hotmail.de>
2018-11-14 10:27:52 +0100

    - Added JS Files - Renamed "Machine Gun"

---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+function sayHello() {
+    console.log("Hello whoever you are!");
+}
+$( document ).ready(function() {
+    console.log( "Document is ready!" );
+    $(".inventoryItem").click(function() {sayHello()});
+});
