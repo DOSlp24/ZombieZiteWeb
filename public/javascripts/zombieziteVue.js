e236629ca24fcbeb28c071b7c6c6dd5c2c1ba58e
MarcBaldi <marc.baldi@hotmail.de>
2019-01-20 13:52:31 +0100

    -vue

--- public/javascripts/zombieziteVue.js
+++ public/javascripts/zombieziteVue.js
@@ -1,28 +1,12 @@
-/*
-     var iIndex = 0;
-    actualPlayer.inventory.forEach(function (theItem) {
-        var itemType = ""
-        if (theItem.name === "Axe" || theItem.name === "Big Mama" || theItem.name === "EVIL SISTERS" || theItem.name === "Flame Thrower" ||
-            theItem.name === "Knife" || theItem.name === "Mashine Gun" || theItem.name === "Pan" || theItem.name === "Pistol" ||
-            theItem.name === "Shotgun" || theItem.name === "Sniper" )
-            itemType = "inventoryWeapon";
-        if (theItem.name === "Boots" || theItem.name === "Chest" || theItem.name === "Healkit" || theItem.name === "Holy Armor" || theItem.name === "Swat-Shield")
-            itemType = "inventoryArmor";
 
-        $("#inventoryContainer").append("<div class=\"col\"><img id='inventory:"+ iIndex+"' class='"+itemType+" inventoryItem' src='/assets/images/items/" + theItem.name + ".png'/>");
-        iIndex = iIndex + 1;
-    });
-
-    if (actualPlayer.inventory.length == 0)
-        $("#topInventoryContainer").append("<p class=\"centered\">Inventory is empty.</p>");
-
-*/
 
 /* --- Vue Stuff - WIP --- */
 
 function VueStuff(result) {
 
-    $("#inventoryContainer").append('<img class="col" v-for="(item, index) in items" :src="item.url" :id="\'inventory:\' + index" :json="setJson({ 'result' })"/>'');
+    /* item und index übergabe klappt ganz gut. Json klappt gar nicht. */
+    /* hier fehler! */
+    $("#inventoryContainer").append('<img class="col" v-for="(item, index) in items" :src="item.url" :id="\'inventory:\' + index" :json="setJson({ 'result' })"/>"');
 
 
 
@@ -33,12 +17,12 @@ function VueStuff(result) {
 
             json: {},
 
-            /*
+            /* Bilder anzeigen geht. */
             items: [
                 { url: '/assets/images/players/K. Kawaguchi.png'},
                 { url: '/assets/images/players/K. Kawaguchi.png'}
             ]
-            */
+            /*items: result.actualPlayer.inventory*/
         },
         methods: {
             setJson (payload) {
@@ -46,7 +30,7 @@ function VueStuff(result) {
             },
         }
 
-            /*items: result.actualPlayer.inventory*/
+
 
     })
 }
